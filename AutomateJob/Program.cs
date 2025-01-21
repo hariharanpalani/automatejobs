bcf6ed3d5cbdccecb8d06b4d973ccf2c5e09e02f
Hari <hariharan@telecomatics.onmicrosoft.com>
2025-01-21 22:58:52 +0530

    first commit

---
+++ AutomateJob/Program.cs
@@ -0,0 +1,126 @@
+﻿using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Presentation;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Microsoft.Data.Analysis;
+using Microsoft.ML;
+// Path to your Excel file
+
+//-i=Ids.xlsx -s=50mb.xlsx -e=Name,Email,Phone,Company,"Job Title"
+
+if (args.Length == 0)
+{
+    throw new ArgumentException("No arguments passed");
+}
+
+string inputFile = "";
+string sourceFile = "";
+List<string> outputColumns = new List<string>();
+
+foreach (var arg in args)
+{
+    var items = arg.Split('=');
+    switch (items[0].ToUpper())
+    {
+        case "--INPUT":
+        case "-I":
+            inputFile = items[1];
+            break;
+        case "-SOURCE":
+        case "-S":
+            sourceFile = items[1];
+            break;
+        case "--EXTRACT":
+        case "-E":
+            outputColumns = items[1].Trim().Split(',').ToList();
+            break;
+    }
+}
+
+Console.WriteLine("input file: {0}", inputFile);
+Console.WriteLine("Source file: {0}", sourceFile);
+Console.WriteLine("Columns to extract: {0}", string.Join(",", outputColumns));
+
+var workbook = new XLWorkbook(sourceFile);
+var worksheet = workbook.Worksheets.First();
+var dataFrame = DataFrameFromWorksheet(worksheet);
+
+var inputbook = new XLWorkbook(inputFile);
+var firstSheet = inputbook.Worksheets.First();
+var columns = firstSheet.ColumnsUsed();
+var fields = new Dictionary<string, List<string>>();
+foreach (var column in columns)
+{
+    var header = column.Cell(1).GetValue<string>();
+    var data = column.CellsUsed().Skip(1).Select(c => c.GetValue<string>()).ToList();
+    fields.Add(header, data);
+}
+
+var conditions = new List<PrimitiveDataFrameColumn<bool>>();
+foreach (var field in fields.Keys)
+{
+    var index = 0;
+    var conditionMask = dataFrame[field].ElementwiseEquals(fields[field][0]); ;
+    foreach (var value in fields[field])
+    {
+        if (index != 0)
+        {
+            conditionMask = (PrimitiveDataFrameColumn<bool>)conditionMask.Or(dataFrame[field].ElementwiseEquals(value));
+        }
+        index++;
+    }
+    conditions.Add(conditionMask);
+}
+
+if (conditions.Count > 0)
+{
+    var combinedMask = conditions[0];
+    var index = 0;
+    foreach (var condition in conditions)
+    {
+        if (index != 0)
+        {
+            combinedMask = (PrimitiveDataFrameColumn<bool>)combinedMask.And(condition);
+        }
+        index++;
+    }
+
+    dataFrame = dataFrame.Filter(combinedMask);
+}
+
+var output = dataFrame.ToDataFrame(outputColumns.ToArray()).ToTable();
+// Display the DataFrame
+var resultBook = new XLWorkbook();
+resultBook.Worksheets.Add(output, "output");
+resultBook.SaveAs("results.xlsx");
+Console.WriteLine(dataFrame);
+
+
+
+
+DataFrame DataFrameFromWorksheet(IXLWorksheet worksheet)
+{
+    var rows = worksheet.RowsUsed();
+    Console.WriteLine("Number of Rows: " + rows.Count());
+    var columns = worksheet.ColumnsUsed();
+    Console.WriteLine("Number of Columns: " + columns.Count());
+    var dataFrameColumns = new DataFrameColumn[columns.Count()];
+
+    int colIndex = 0;
+    foreach (var column in columns)
+    {
+        var header = column.Cell(1).GetValue<string>();
+        /* var data = column.Cells($"2:{rows.Count() - 1}").Select(cell =>
+         {
+             string? content = null;
+             cell.TryGetValue<string>(out content);
+             return content;
+         }).ToList();*/
+        var data = column.Cells($"2:{rows.Count()}").Select(cell => cell.IsEmpty() ? null : cell.GetValue<string>()).ToList();
+        //data = PadData(data, rows.Count() - 1);
+        var dataColumn = new StringDataFrameColumn(header, data);
+        //dataColumn = PadColumn(dataColumn, rows.Count());
+        dataFrameColumns[colIndex++] = dataColumn;
+    }
+
+    return new DataFrame(dataFrameColumns);
+}
---
+++ AutomateJob/Sample.cs
@@ -0,0 +1,78 @@
+﻿using ClosedXML.Excel;
+using Microsoft.Data.Analysis;
+using Microsoft.ML;
+class Program1
+{
+     void Main(string[] args)
+    {
+        // Path to your Excel file
+        string filePath = "50mb.xlsx";
+
+        // Load Excel file using ClosedXML
+        var workbook = new XLWorkbook(filePath);
+        var worksheet = workbook.Worksheets.First();
+        var dataFrame = DataFrameFromWorksheet(worksheet);
+        string[] allowedNames = { "Able Seamen", "Account Manager" };
+        string[] allowedCities = { "Tremblay, Crona and Hagenes", "Christiansen, Shields and Ernser", "Abbott PLC" };
+
+        var nameMask = dataFrame["Job Title"].ElementwiseEquals(allowedNames[0]);
+        for (int i = 1; i < allowedNames.Length; i++)
+        {
+            nameMask = (PrimitiveDataFrameColumn<bool>)nameMask.Or(dataFrame["Job Title"].ElementwiseEquals(allowedNames[i]));
+        }
+
+        var cityMask = dataFrame["Company"].ElementwiseEquals(allowedCities[0]);
+        for (int i = 1; i < allowedCities.Length; i++)
+        {
+            cityMask = (PrimitiveDataFrameColumn<bool>)cityMask.Or(dataFrame["Company"].ElementwiseEquals(allowedCities[i]));
+        }
+        // dataFrame = dataFrame.Filter(dataFrame["Id"].v);
+
+        var combinedMask = (PrimitiveDataFrameColumn<bool>)nameMask.And(cityMask);
+        // Filter the DataFrame
+        var filteredDf = dataFrame.Filter(combinedMask);
+
+        var output = filteredDf.ToDataFrame("Name", "Company", "Job Title", "Address", "Email").ToTable();
+        // Display the DataFrame
+        var resultBook = new XLWorkbook();
+        resultBook.Worksheets.Add(output, "output");
+        resultBook.SaveAs("results.xlsx");
+        Console.WriteLine(dataFrame);
+    }
+
+    static DataFrame DataFrameFromWorksheet(IXLWorksheet worksheet)
+    {
+        var rows = worksheet.RowsUsed();
+        Console.WriteLine("Number of Rows: " + rows.Count());
+        var columns = worksheet.ColumnsUsed();
+        Console.WriteLine("Number of Columns: " + columns.Count());
+        var dataFrameColumns = new DataFrameColumn[columns.Count()];
+
+        int colIndex = 0;
+        foreach (var column in columns)
+        {
+            var header = column.Cell(1).GetValue<string>();
+            var data = column.CellsUsed().Skip(1).Select(cell => cell.GetValue<string>()).ToList();
+            data = PadData(data, rows.Count() - 1);
+            var dataColumn = new StringDataFrameColumn(header, data);
+            //dataColumn = PadColumn(dataColumn, rows.Count());
+            dataFrameColumns[colIndex++] = dataColumn;
+        }
+
+        return new DataFrame(dataFrameColumns);
+    }
+
+    static List<string> PadData(List<string> data, int maxLength)
+    {
+        if (data.Count < maxLength)
+        {
+            var startIndex = data.Count + 1;
+            while (startIndex <= maxLength)
+            {
+                data.Add("");
+                startIndex++;
+            }
+        }
+        return data;
+    }
+}
